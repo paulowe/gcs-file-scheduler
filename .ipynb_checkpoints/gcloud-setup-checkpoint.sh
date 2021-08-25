4a913ef0a9498e749cc5ea05401ec18faaa0d7d6
Paul C. Owe <40435982+owecodes@users.noreply.github.com>
2021-08-25 18:44:33 -0400

    updates

---
+++ .ipynb_checkpoints/gcloud-setup-checkpoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Make sure to download google cloud sdk locally on your machine
+
+../google-cloud-sdk/bin/gcloud init
+../google-cloud-sdk/bin/gcloud auth application-default login
+
